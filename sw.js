ff0125a3f99f565c153fcfe0942e0ac958bb7229
ShafiurShamim <shafiurshamim@gmail.com>
2020-01-27 03:04:29 +0600

    Added dynamic cache features

--- sw.js
+++ sw.js
@@ -1,11 +1,24 @@
 const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+
 const assets = [
     '/',
     '/favicon.ico',
     '/index.html',
     '/css/app.css',
-    '/js/app.js'
+    '/js/app.js',
+    '/pages/fallback.html'
 ];
+// limit cache size
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
+    })
+};
 // service worker events
 // install event
 self.addEventListener("install", evt => {
@@ -30,7 +43,7 @@ self.addEventListener("activate", evt => {
             // when all of the promises passed as an iterable have been fulfilled or when the iterable contains no promises.
             // It rejects with the reason of the first promise that rejects.
             return Promise.all(cacheKeys
-                .filter(key => key !== staticCacheName)
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
                 .map(key => caches.delete(key))
             )
         })
@@ -47,7 +60,16 @@ self.addEventListener("fetch", evt => {
         // associated with the first matching request in the Cache object. If no match is found, the Promise resolves to undefined.
         caches.match(evt.request).then(cacheResponse => {
             // Returns cacheResponse; else, fetch resources of the request.
-            return cacheResponse || fetch(evt.request);
+            return cacheResponse || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    limitCacheSize(dynamicCacheName, 3);
+                    return fetchRes;
+                })
+            });
+        }).catch(() => {
+            // console.log(evt.request.url);
+            return caches.match('/pages/fallback.html')
         })
     );
 
